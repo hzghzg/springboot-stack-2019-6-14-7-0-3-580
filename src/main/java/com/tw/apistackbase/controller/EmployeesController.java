461b738b602e3b4a25fc531e17c8ca099ce92af2
DARYL HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGDA7@OOCL.COM>
2019-07-16 20:13:18 +0800

    feat:add a delete  method name deleteEmployee

--- src/main/java/com/tw/apistackbase/controller/EmployeesController.java
+++ src/main/java/com/tw/apistackbase/controller/EmployeesController.java
@@ -5,16 +5,25 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/employees")
 public class EmployeesController {
-    private List<Employee> employees=new ArrayList<>();
+    private List<Employee> employees=Employee.createEmployeesList();
     @PostMapping()
     public Employee addEmployees(@RequestBody Employee employee){
             employees.add(employee);
             return employee;
-        }
     }
+
+    @DeleteMapping
+    public List<Employee> deleteEmployee(@RequestParam int id){
+        employees.forEach(item->{
+            if(item.getId()==id) employees.remove(item);
+        });
+        return employees;
+    }
+}
 
 
--- src/main/java/com/tw/apistackbase/model/Employee.java
+++ src/main/java/com/tw/apistackbase/model/Employee.java
@@ -9,6 +9,13 @@ public class Employee {
     private int age;
     private String gender;
 
+    public Employee(int id, String name, int age, String gender) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
     public int getId() {
         return id;
     }
@@ -40,5 +47,11 @@ public class Employee {
     public void setGender(String gender) {
         this.gender = gender;
     }
-
+    public static List<Employee> createEmployeesList(){
+        List<Employee> employeesList=new ArrayList<>();
+        employeesList.add(new Employee(0,"Xiaoming",20,"Male"));
+        employeesList.add(new Employee(1,"Xiaohong",19,"Female"));
+        employeesList.add(new Employee(2,"Xiaozhi",15,"Male"));
+        return employeesList;
+    }
 }
